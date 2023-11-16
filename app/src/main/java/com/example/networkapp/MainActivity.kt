7b598202842e7bee111eb1a52adcf2ab1b8af6ab
KevinXJarema <tun48388@temple.edu>
2023-11-16 15:19:25 -0500

    created  svae Comic method, loading is done in the on create function and the comic is saved with when it is downloaded for loading

--- app/src/main/java/com/example/networkapp/MainActivity.kt
+++ app/src/main/java/com/example/networkapp/MainActivity.kt
@@ -1,5 +1,6 @@
 package com.example.networkapp
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
@@ -14,6 +15,16 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.squareup.picasso.Picasso
 import org.json.JSONObject
+import android.content.SharedPreferences
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileNotFoundException
+import java.io.FileOutputStream
+import java.io.FileReader
+import java.io.IOException
+import java.io.InputStreamReader
+import java.lang.Exception
+import java.lang.StringBuilder
 
 // TODO (1: Fix any bugs)
 // TODO (2: Add function saveComic(...) to save and load comic info automatically when app starts)
@@ -26,12 +37,16 @@ class MainActivity : AppCompatActivity() {
     lateinit var numberEditText: EditText
     lateinit var showButton: Button
     lateinit var comicImageView: ImageView
-
+    lateinit var sharedPreferences: SharedPreferences
+    private val internalFilename = "my_file"
+    private lateinit var file : File
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        file = File(filesDir, internalFilename)
         requestQueue = Volley.newRequestQueue(this)
+        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
 
         titleTextView = findViewById<TextView>(R.id.comicTitleTextView)
         descriptionTextView = findViewById<TextView>(R.id.comicDescriptionTextView)
@@ -43,12 +58,34 @@ class MainActivity : AppCompatActivity() {
             downloadComic(numberEditText.text.toString())
         }
 
+
+        if(file.exists()){
+            try {
+                val br = BufferedReader(FileReader(file))
+                val text = StringBuilder()
+                var line: String?
+                while (br.readLine().also { line = it } != null) {
+                    text.append(line)
+                }
+                br.close()
+                val jsonObj = JSONObject(text.toString())
+                showComic(jsonObj)
+            }catch(e : IOException){
+                e.printStackTrace()
+            }
+
+            }
+        showButton.setOnClickListener(){
+            downloadComic(numberEditText.toString())
+        }
+
     }
+
 
     private fun downloadComic (comicId: String) {
         val url = "https://xkcd.com/$comicId/info.0.json"
         requestQueue.add (
-            JsonObjectRequest(url, {showComic(it)}, {
+            JsonObjectRequest(url, {showComic(it); saveComic(it)}, {
             })
         )
     }
@@ -57,6 +94,19 @@ class MainActivity : AppCompatActivity() {
         titleTextView.text = comicObject.getString("title")
         descriptionTextView.text = comicObject.getString("alt")
         Picasso.get().load(comicObject.getString("img")).into(comicImageView)
+    }
+
+    private fun saveComic(comicObject: JSONObject){
+       try{
+           val outputStream = FileOutputStream(file)
+           outputStream.write(comicObject.toString().toByteArray())
+           outputStream.close()
+
+       }catch(e: Exception){
+           e.printStackTrace()
+       }
+
+
     }
 
 
